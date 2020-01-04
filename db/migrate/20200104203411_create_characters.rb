2d0062b35b1e5d122847678b4cedf9a17e836f7d
Zachary Schulz <zjschulz@gmail.com>
2020-01-04 20:36:10 +0000

    Fixed migrations

--- db/migrate/20200104203411_create_characters.rb
+++ db/migrate/20200104203411_create_characters.rb
@@ -1,4 +1,4 @@
-class CreateCharacters < ActiveRecord::Migration[6.0]
+class CreateCharacters < ActiveRecord::Migration
   def change
     create_table :characters do |t|
       t.string :player_name
---
+++ db/schema.rb
@@ -0,0 +1,34 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20200104203531) do
+
+  create_table "characters", force: :cascade do |t|
+    t.string  "player_name"
+    t.string  "campaign"
+    t.string  "character_name"
+    t.string  "character_class"
+    t.integer "armor_class"
+    t.integer "perception"
+    t.integer "investigation"
+    t.integer "insight"
+    t.integer "spell_save_dc"
+    t.string  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+  end
+
+end
