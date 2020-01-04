ae64e4b0157cb48c989e3379e006a9bc6662847a
Zachary Schulz <zjschulz@gmail.com>
2020-01-04 21:00:46 +0000

    Mapped out CRUD actions

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,31 @@
+class CharactersController < ApplicationController
+  
+  #CREATE
+  
+    #NEW
+    #get request to '/'
+  
+    #CREATE
+    #post request to /characters
+    
+  #READ
+  
+    #INDEX
+    #get request to /characters
+    
+    #SHOW
+    #get request to /characters/:id
+  
+  #UPDATE
+  
+    #EDIT
+    #get request to /characters/:id/edit
+    
+    #UPDATE
+    #patch request to /characters/:id
+  
+  #DELETE
+  
+    #delete request to /characters/:id
+  
+end
